f2ed2b1c3a5609b0584c2159fa5ae0b6ea04e236
Priynshu2341 <priyanshukushwaha26@gmail.com>
2026-03-13 04:48:17 +0530

    added api

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const backendApi = axios.create({
-    baseURL: "http://localhost:8080/api/v1"
+    baseURL: "https://ecommerce-backend-x2cb.onrender.com/api/v1"
 })
 
 backendApi.interceptors.request.use((config) =>{
